f84aa2b57dc56a07bf0e6d792cadcd197dbef61b
rand00 <oth.rand@gmail.com>
2014-10-05 06:54:34 +0200

    Initial random ai

---
+++ ai.ml
@@ -0,0 +1,19 @@
+open Batteries 
+open Core_rand00
+open Gametypes
+
+module Random = struct 
+
+  let random_element player_id (pct_lambda, pct_symbol) = 
+    let rand = Random.float 1. in
+    if rand < pct_lambda then
+      Lambda (player_id, [random_symbol ()], [random_symbol ()])
+    else if rand < (pct_lambda +. pct_symbol) then
+      Symbol (player_id, random_symbol ())
+    else Empty
+
+  let next_move player_id _ = 
+    let pct_lambda, pct_symbol = (0.1, 0.5)
+    in random_element player_id (pct_lambda, pct_symbol)
+
+end
