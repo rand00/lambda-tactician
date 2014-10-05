3e415ff950bb8b895757093aca612834179fba47
rand00 <oth.rand@gmail.com>
2014-10-06 00:59:00 +0200

    Initial test-ai-setup

--- lambdacommando.ml
+++ lambdacommando.ml
@@ -2,6 +2,36 @@ open Batteries
 open Core_rand00 
 open Gametypes
 
+let _ = 
+
+  let gstate = {
+    winner = None;
+    board = Array.make 16 Empty;
+
+    p0 = {
+      id = P0; name = "Hansi";
+      location = Local;
+      position = `Left;
+      next_move = Ai.Random.next_move;
+      mana = 1.;
+    };
+
+    p1 = {
+      id = P1; name = "Finka";
+      location = Local;
+      position = `Right;
+      next_move = Ai.Random.next_move;
+      mana = 1.;
+    };
+  } in 
+  
+  in 
+  Control.gloop gstate
+    ~next_fun:Board.next
+    ~visualizer:Visualizer.termprinter0 
+    
+    
+
 
 
 
