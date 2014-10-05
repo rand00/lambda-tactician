ce5d9866cfe8c54586bf72fe706ff35c84455c65
rand00 <oth.rand@gmail.com>
2014-10-06 00:57:04 +0200

    Element owner-id's saved in wrapper now

--- ai.ml
+++ ai.ml
@@ -11,17 +11,17 @@ let random_symbol () =
 
 module Random = struct 
 
-  let random_element player_id (pct_lambda, pct_symbol) = 
+  let random_element (pct_lambda, pct_symbol) = 
     let rand = Random.float 1. in
     if rand < pct_lambda then
-      Lambda (player_id, [random_symbol ()], [random_symbol ()])
+      Lambda (random_symbol (), random_symbol ())
     else if rand < (pct_lambda +. pct_symbol) then
-      Symbol (player_id, random_symbol ())
+      Symbol (random_symbol ())
     else Empty
 
   (*goto: use lwt here?*)
-  let next_move player_id _ = 
+  let next_move _ _ = 
     let pct_lambda, pct_symbol = (0.1, 0.5)
-    in random_element player_id (pct_lambda, pct_symbol)
+    in random_element (pct_lambda, pct_symbol)
 
 end
