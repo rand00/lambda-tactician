8ac1bdaaa729a59c33f39f7c04622c2c0f220857
rand00 <oth.rand@gmail.com>
2014-10-07 16:58:16 +0200

    License, todo's and fix fun. application.

--- control.ml
+++ control.ml
@@ -1,16 +1,56 @@
+(*
+  LambdaTactician - a cmd-line tactical lambda game.
+  Copyright (C) 2014 Claes Worm 
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
 open Batteries
 open Core_rand00 
 open Gametypes
 
-let gloop gstate_init ~next_fun ~visualizer = 
-  let next = next_function visualizer
-  in
+let gstep gstate = 
+  ()
+(*goto grab user-action(s?) for this turn*)
+
+(*goto determine legal turn action based on some new user-action type *)
+
+(*goto: iterate throgh (map) and remove earlier killed elems (they are there for last-turn visuals)*)
+
+(*moving the current players elems*)
+(*goto 
+  . only do this if owner-actions allows (a path in the user-action tree; make new type)
+  . return actions + new-array*)
+(*goto rewrite ; 
+  match on input-actions (based on rules)
+  . don't control direction from here - let rules-mod choose and pmatch!
+  . let rules-mod return the action to be done on some*)
+(*goto action allows for it; add new elem from curr player (if relevant) + add this to actions*)
+
+(*goto calculate mana-costs/-income from 'actions', and later return mana+winner-gamestate*)
+
+(*goto visualize*)
+
+
+
+(* ~rules ~visualizer ~iinterp ; firstclass mod's*)
+let gloop gstate_init = 
   let rec aux = function
     | { winner = Some player; p0; p1 } -> 
       (match player with 
-       | P0 -> print_endline "And the winner is "^p0.name^"!"
-       | P1 -> print_endline "And the winner is "^p1.name^"!")
-    | gstate -> gameloop (next gstate)
+       | P0 -> print_endline ("And the winner is "^p0.name^"!")
+       | P1 -> print_endline ("And the winner is "^p1.name^"!"))
+    | gstate -> aux (gstep gstate)
   in 
   aux gstate_init
 
