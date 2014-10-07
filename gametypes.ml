3ea260f3599cf418450624952a610e46133d14b3
rand00 <oth.rand@gmail.com>
2014-10-07 16:59:22 +0200

    License and added player-action-types.

--- gametypes.ml
+++ gametypes.ml
@@ -1,3 +1,20 @@
+(*
+  LambdaTactician - a cmd-line tactical lambda game.
+  Copyright (C) 2014 Claes Worm 
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
 
 type symbol = X | Y | Z
 
@@ -40,6 +57,10 @@ type game_state = {
   board : game_board;
   winner : player_id option;
 }
+
+type player_action = 
+  | Move_all_and_add of element
+  | Move_all_backward
 
 type game_action = 
   | Kill of element
