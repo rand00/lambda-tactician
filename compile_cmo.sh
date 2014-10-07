8318f7b0cd6a85a9207291fba8f6c41d0f8dc689
rand00 <oth.rand@gmail.com>
2014-10-07 21:33:11 +0200

    Added new modules to compilescript

--- compile_cmo.sh
+++ compile_cmo.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 ocamlbuild -use-ocamlfind -no-links -Is core_rand \
-    gametypes.cmo visualizer.cmo control.cmo ai.cmo iinterp.cmo #rules.cmo
+    gametypes.cmo visualizer.cmo board.cmo player.cmo control.cmo ai.cmo iinterp.cmo #rules.cmo
 
