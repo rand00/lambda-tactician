fb1b16f131471c6832a8122fd61cf2c0aa3bc1f5
rand00 <oth.rand@gmail.com>
2014-10-09 19:50:49 +0200

    License-add and change args Gstate.next_move

--- gstate.ml
+++ gstate.ml
@@ -1,3 +1,20 @@
+(*
+  LambdaTactician - a cmd-line tactical lambda game.
+  Copyright (C) 2014 Claes Worm 
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
 open Batteries
 open Core_rand00
 open Gametypes
@@ -22,10 +39,10 @@ let opposite_direction = function
   | Left -> Right
   | Right -> Left
 
-let next_move gstate pid = 
+let next_move gstate = 
   let open Player in
   let module R = (val gstate.rules)
-  in match pid with 
+  in match gstate.turn with 
   | P0 -> 
     let next_elem = 
       gstate.p0.next_move 
