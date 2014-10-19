403ac73e51ef0550e8fddb7af51fdcf3003737da
rand00 <oth.rand@gmail.com>
2014-10-19 17:51:48 +0200

    Added simple visualization of gameboard.

--- visualizer.ml
+++ visualizer.ml
@@ -15,9 +15,39 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *)
+
 open Batteries
 open Core_rand00 
 open Gametypes
 
-let termprinter0 = print_string "\rwall"
+module type S = sig 
+  val board : gstate:Gstate.t -> unit
+end
+
+module Basic : S = struct 
+
+  open Gstate
+  open Player
+
+  let str_of_sym = function 
+    | X -> "x" | Y -> "y" | Z -> "z"
+
+  let sep, fill = "|", "_" 
+
+  let str_of_elem = function 
+    | Lambda (s0, s1) -> [ str_of_sym s0; "."; str_of_sym s1 ]
+    | Symbol s -> [ fill; str_of_sym s; fill ]
+    | Empty -> List.make 3 fill
+
+  let board ~gstate = 
+    let elems = List.of_enum (Board.enum gstate.board) in
+    let board = String.concat ""
+        (List.concat 
+           ((List.fold_right (fun elem acc -> 
+                [sep] :: (str_of_elem elem.element) :: acc )
+             ) elems [[sep]] )) in
+    let full = ((*"\r" ^*) board)
+    in print_string full
+
+end
 
