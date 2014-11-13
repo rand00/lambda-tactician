22357940bfa5bf521a884141b3487f58fac9a635
rand00 <oth.rand@gmail.com>
2014-11-14 00:27:45 +0100

    Setup basic osc client-msg test-case

--- tests/osc_comm0.ml
+++ tests/osc_comm0.ml
@@ -1,5 +1,27 @@
 
 open Batteries 
 open Core_rand00
+open Lwt
+open Osc_lwt.Udp
+
+
+let test () = 
+  let localhost = Unix.inet_addr_of_string "127.0.0.1" 
+  and port = 57110 in
+  let addr = Lwt_unix.ADDR_INET (localhost, port) in
+  let packet = Osc.(Message {
+      address = "/s_newargs"; (*goto change to some useful sig*)
+      arguments = [];
+    })
+  in
+  Client.create () >>= fun client ->
+  Client.send client addr packet
+  >> Client.destroy client
+  
+
+let _ = test ()
+
+
+
 
 
