f8048d40b11591b322bfc6d789f8fbaf89f182bb
rand00 <oth.rand@gmail.com>
2014-11-14 23:23:59 +0100

    OSC test updated with setting freq + stopping synth

--- tests/osc_comm0.ml
+++ tests/osc_comm0.ml
@@ -9,24 +9,50 @@ let test () =
   let localhost = Unix.inet_addr_of_string "127.0.0.1" 
   and port = 57110 in
   let addr = Lwt_unix.ADDR_INET (localhost, port) in
-  let packet = Osc.(Message {
-      address = "/s_new"; (*goto change to some useful sig*)
+  let n_id = 1001l
+  in
+  let start_synth = Osc.(Message {
+      address = "/s_new"; (*use of '/' instead of '\' from sc-lang definitions*)
       arguments = [
         String "sinew"; (*synthdef name*)
-        Int32 1001l; (*node id*)
+        Int32 n_id; (*node id*)
         Int32 0l; (*?*)
         Int32 0l; (*?*)
         String "vol"; (*synthdef parameter*)
         Float32 1.1; (*parameter value*)
       ];
     })
-  in
-  Client.create () >>= fun client ->
-  Client.send client addr packet
-  >> Client.destroy client
-  
+  and change_freq = Osc.(Message {
+      address = "/n_set";
+      arguments = [
+        Int32 n_id;
+        String "freq";
+        Int32 431l;
+      ]
+    })
+  and stop_synth = Osc.(Message {
+      address = "/n_free";
+      arguments = [ Int32 n_id ]
+    })
+  in 
+  Client.create ()
+  >>= fun client ->
 
-let _ = test ()
+  Lwt_io.printl "Starting synth"
+  >> Client.send client addr start_synth
+  >> Lwt_unix.sleep 1.
+
+  >> Lwt_io.printl "Changing frequency"
+  >> Client.send client addr change_freq
+  >> Lwt_unix.sleep 1.
+
+  >> Lwt_io.printl "Stopping synth"
+  >> Client.send client addr stop_synth
+  >> Lwt_unix.sleep 1.    
+  >> Client.destroy client
+
+
+let _ = Lwt_main.run (test ())
 
 
 
