92c23819278c4d7af1d43dbacb728d598b739838
rand00 <oth.rand@gmail.com>
2015-08-27 22:43:17 +0200

    Running simple example of new anim code

--- src/anim.ml
+++ src/anim.ml
@@ -10,8 +10,8 @@ type 'a anim =
 (**Some internal representation of animation-elements*)
 
 type 'a state = {
-  p : 'a option;
   c : 'a;
+(*  prev : 'a option;*)
 }
 
 let map_rules e = function 
@@ -39,36 +39,59 @@ open Lwt_react
 let (>|~) = (fun e f -> E.map f e)
 
 let frames, send_frame = E.create () 
-let print_frame = E.map (Lwt_io.printf "%d\n") frames
+(*let print_frame = E.map (Lwt_io.printf "%d\n") frames*)
+
+let std_state = {
+  c = ""; 
+}
+
+let anim = frames >|~ fun frame_n -> 
+  Al ([ 
+      Ae ({ std_state with c = "[" }, None);
+      Ae ({ std_state with c = "" },
+         Some (fun _ -> match frame_n mod 4 with
+              | 0 -> { c = "   " }
+              | 1 -> { c = "|  " }
+              | 2 -> { c = "|| " }
+              | 3 -> { c = "|||" }
+            ));
+      Ae ({ std_state with c = "]" }, None);
+    ], None)
+  |> incr_anim
+
+let eval_anim = anim >|~ eval ~cat:(^) ~get:(fun {c} -> c) 
+
+let print_anim = eval_anim >|~ Lwt_io.printf "%s\n"
 
 let () = 
   let frame_n () =
     let rec aux n = 
       let () = send_frame n in
-      Lwt_unix.sleep 0.04 >> aux (if n > 1000 then 0 else succ n)
+      Lwt_unix.sleep 0.04 >> aux (if n = 1000 then 0 else succ n)
     in aux 0
   in Lwt.async (fun () -> (return (Lwt_main.run (frame_n ()))))
 
 (**Tests*)
 
+(*
 let run () =
   (*goto parametrize animation, and lift it over frp dep's*)
   let anim = 
     Al ([ 
-        Ae ({ p = None; c = "["}, 
+        Ae ({ std_state with c = "[" }, 
             Some ( function 
-                | {p = None; c} -> {p = Some c; c = "Q"}
-                | {p = Some c'; c} -> {p = Some c; c = c'} 
+                | {c; prev = None;}   -> {c = "Q"; prev = Some c}
+                | {c; prev = Some c'} -> {c = c'; prev = Some c} 
               ));
-        Ae ({ p = None; c = "|||"}, None);
-        Ae ({ p = None; c = "]"}, 
+        Ae ({ std_state with c = "|||" }, None);
+        Ae ({ std_state with c = "]" }, 
             Some ( function         
-                | {p = None; c} -> {p = Some c; c = "Z"}
-                | {p = Some c'; c} -> {p = Some c; c = c'}
+                | {prev = None; c} -> {prev = Some c; c = "Z"}
+                | {prev = Some c'; c} -> {prev = Some c; c = c'}
               ));
       ], None)
   in 
-  let eval = eval ~cat:(^) ~get:(fun {c} -> c) in
+  let eval_anim = eval ~cat:(^) ~get:(fun {c} -> c) in
   let rec loop anim = function
     | 0 -> ()
     | n -> begin
@@ -76,7 +99,7 @@ let run () =
         loop (incr_anim anim) (pred n)
       end
   in loop anim 10 
-
+*)
 
 
 
