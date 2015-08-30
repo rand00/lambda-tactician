b88d3b307db1c342632e4bbd72ce172bbd2844cd
rand00 <oth.rand@gmail.com>
2015-08-30 19:27:51 +0200

    Moved test-code out and made an internal module T for opening anim types in other modules (for not to litter scopes)

--- src/anim.ml
+++ src/anim.ml
@@ -2,32 +2,33 @@ open Batteries
 
 (**Abstract type of animations*)
 
-type 'a rules = 'a -> 'a
+module T = struct
 
-type 'a anim = 
-  | Al of 'a anim list * 'a anim list rules option
-  | Ae of 'a * 'a rules option
+  type 'a rules = 'a -> 'a
 
-(**Some internal representation of animation-elements*)
+  (*The 'a type parameter is for saving state about the animation-elements (Ae's)*)
+  type 'a anim = 
+    | Al of 'a anim list * 'a anim list rules option
+    | Ae of 'a * 'a rules option
 
-type 'a state = {
-  c : 'a;
-(*  prev : 'a option;*)
-}
+end
 
-let std_state = {
-  c = ""; 
-}
+open T
 
-let rec state_eq s s' = match s, s' with
-  | Al (le, _), Al (le', _) -> begin
-      try List.fold_right2 (fun e e' eq -> 
-          eq && state_eq e e'
-        ) le le' true
-      with LazyList.Different_list_size _ -> false
-    end
-  | Ae (state, _), Ae (state', _) -> state = state'
-  | _ -> false
+(**Evaluation*)
+
+let state_eq ~eq s s' = 
+  let rec aux s s' = 
+    match s, s' with
+    | Al (le, _), Al (le', _) -> begin
+        try List.fold_right2 (fun e e' acc_eq -> 
+            acc_eq && aux e e'
+          ) le le' true
+        with LazyList.Different_list_size _ -> false
+      end
+    | Ae (state, _), Ae (state', _) -> eq state state'
+    | _ -> false
+  in aux s s'
 
 let map_rules e = function 
   | Some r -> r e
@@ -46,104 +47,6 @@ let eval ~cat ~get =
     | Al ([], _) -> failwith "Empty animation!"
   in aux 
 
-(**FRP deps for anims*)
-
-open Lwt
-open Lwt_react
-
-let (>|~) = (fun e f -> S.map f e)
-
-let frames, send_frame = E.create ()
-(*let print_frame = S.map (Lwt_io.printf "%d\n") frames*)
-
-let frames_s = S.hold 0 frames
-
-(*goto this is an interesting but weird definition where both a signal and event of frames is used... 
-  - how to make more clean alternative? 
-      => fix need a function that will give same value on second update step (with equal input as before)*)
-let anim = 
-  let anim_def = 
-    Al ([ 
-        Ae ({ std_state with c = "[" }, Some (function 
-            | { c = "[" } -> { c = "-" }
-            | _ -> { c = "[" }
-          ));
-        Ae ({ std_state with c = "" },
-            Some (fun _ -> match S.value frames_s mod 4 with
-                | 0 -> { c = "   " }
-                | 1 -> { c = "|  " }
-                | 2 -> { c = "|| " }
-                | 3 -> { c = "|||" }
-              ));
-        Ae ({ std_state with c = "]" }, None);
-      ], None)
-    |> incr_anim
-  in S.fold ~eq:state_eq (fun anim_acc _ -> incr_anim anim_acc) anim_def frames
-
-(*
-let anim = 
-  let anim_def = 
-    Al ([ 
-        Ae ({ std_state with c = "[" }, Some (function 
-            | { c = "[" } -> { c = "-" }
-            | _ -> { c = "[" }
-          ));
-        Ae ({ std_state with c = "" },
-            Some (fun _ -> match S.value frames mod 4 with
-                | 0 -> { c = "   " }
-                | 1 -> { c = "|  " }
-                | 2 -> { c = "|| " }
-                | 3 -> { c = "|||" }
-              ));
-        Ae ({ std_state with c = "]" }, None);
-      ], None)
-  in S.fix ~eq:state_eq (anim_def) (fun s -> 
-      let s' = S.map ~eq:state_eq (fun anim -> incr_anim anim ) s
-      in s', s'
-    )
-*)
-
-let eval_anim = anim >|~ eval ~cat:(^) ~get:(fun {c} -> c) 
-
-let print_anim = eval_anim >|~ (fun s -> Sys.command "tput cuu1" |> ignore; Lwt_io.printf "%s\n" s)
-
-let () = 
-  let frame_n () =
-    let rec aux n = 
-      let () = send_frame n in
-      Lwt_unix.sleep 0.04 >> aux (if n = 1000 then 0 else succ n)
-    in aux 0
-  in Lwt.async (fun () -> (return (Lwt_main.run (frame_n ()))))
-
-(**Tests*)
-
-(*
-let run () =
-  (*goto parametrize animation, and lift it over frp dep's*)
-  let anim = 
-    Al ([ 
-        Ae ({ std_state with c = "[" }, 
-            Some ( function 
-                | {c; prev = None;}   -> {c = "Q"; prev = Some c}
-                | {c; prev = Some c'} -> {c = c'; prev = Some c} 
-              ));
-        Ae ({ std_state with c = "|||" }, None);
-        Ae ({ std_state with c = "]" }, 
-            Some ( function         
-                | {prev = None; c} -> {prev = Some c; c = "Z"}
-                | {prev = Some c'; c} -> {prev = Some c; c = c'}
-              ));
-      ], None)
-  in 
-  let eval_anim = eval ~cat:(^) ~get:(fun {c} -> c) in
-  let rec loop anim = function
-    | 0 -> ()
-    | n -> begin
-        print_endline (eval anim);
-        loop (incr_anim anim) (pred n)
-      end
-  in loop anim 10 
-*)
 
 
 
