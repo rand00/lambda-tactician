76fddcb78740eaf10bcd2f3e0b1aa0fb336ad5b2
rand00 <oth.rand@gmail.com>
2015-10-01 00:36:13 +0200

    Tried out running ocamldebug on program - no luck (need to recompile all opam pkgs with -g??)

--- compile_lambtac_byte.sh
+++ compile_lambtac_byte.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 #important to use -Is instead of -I
-ocamlbuild -use-ocamlfind -cflag -bin-annot \
+ocamlbuild -use-ocamlfind \
+    -cflag -bin-annot \
+    -cflag -g \
     -Is src,lib_batext_rand00/src,lib_sc/src \
     lambdatactician.byte
