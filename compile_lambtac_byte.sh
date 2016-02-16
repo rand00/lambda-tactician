18e3c1e9154b8e927feff2c4eaae5467bf426aab
rand00 <oth.rand@gmail.com>
2016-02-16 15:45:03 +0100

    Removed redundant flags from compile-script

--- compile_lambtac_byte.sh
+++ compile_lambtac_byte.sh
@@ -2,7 +2,7 @@
 
 #important to use -Is instead of -I
 ocamlbuild -use-ocamlfind \
-    -cflag -bin-annot \
-    -cflag -g \
     -Is src,lib_batext_rand00/src,lib_sc/src \
     lambdatactician.byte
+#    -cflag -bin-annot \
+#    -cflag -g \
